b461cb0f2d27a81ca279fc1d002213de17842a5c
EngincanOzkan <engin.can.eng@gmail.com>
2021-08-23 23:19:42 +0300

    hp-2 ui tarafında auth yapılması

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ClientApp';
+
+  get isUserAuthenticated(): boolean {
+    const token: string | null = localStorage.getItem('jwt');
+    if(token){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  logOut() {
+    localStorage.removeItem("jwt");
+  }
+}
